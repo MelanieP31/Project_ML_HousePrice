c2049aca317e457cf9a0e36c2079005f5d69376c
MelanieP31 <melanie.picot01@gmail.com>
2024-06-08 17:48:11 +0200

    FlaskServer
    Test postman ok !

--- House_Price_Toulouse/server/server.py
+++ House_Price_Toulouse/server/server.py
@@ -2,14 +2,39 @@ from flask import Flask, request, jsonify
 import util
 app = Flask(__name__)
 
-@app.route('/get_location_name')
-def get_location_name():
+@app.route('/get_locations', methods=['GET'])
+def get_locations():
     response = jsonify({
-        'code_postal': util.get_location_name()
+        'locations': util.get_location_name()
     })
     response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
+@app.route('/predict_price', methods=['POST'])
+def predict_price():
+    try :
+        data = request.get_json()
+        location = data['location']
+        surface_reelle_bati = int(data['surface_reelle_bati'])
+        nombre_pieces_principales = int(data['nombre_pieces_principales'])
+        surface_terrain = float(data['surface_terrain'])
+
+        estimated_price = util.get_estimated_price(location, surface_reelle_bati, nombre_pieces_principales, surface_terrain)
+        response = jsonify({
+            'estimated_price': estimated_price
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    except Exception as e:
+        print(f"Error: {e}")
+        response = jsonify({
+            'error': str(e)
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 500
+
+
+
 if __name__ == "__main__":
-    print("Starting Python Flask Server Form Home Price Prediction ...")
-    app.run()
+    util.load_saved_artefacts()
+    app.run(debug=True)
--- House_Price_Toulouse/server/util.py
+++ House_Price_Toulouse/server/util.py
@@ -1,10 +1,26 @@
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns=None
 __model=None
 
+def get_estimated_price(location, surface_reelle_bati, nombre_pieces_principales, surface_terrain) :
+    try :
+        loc_index = __data_columns.index(location.lower())
+    except :
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = surface_reelle_bati
+    x[1] = nombre_pieces_principales
+    x[2] = surface_terrain
+
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return float(round(__model.predict([x])[0],2))
 
 def get_location_name():
     return __locations
@@ -14,9 +30,15 @@ def load_saved_artefacts():
     global __data_columns
     global __locations
 
-    with open("./artifacts/columns.json","r") as f:
+    with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
+    global __model
     with open("./artifacts/house_price_hautegar_model.pickle","rb") as f:
         __model = pickle.load(f)
+
+if __name__ == '__main__' :
+    load_saved_artefacts()
+    print(get_location_name())
+    print(get_estimated_price("code_postal_31100",100,2,0))
